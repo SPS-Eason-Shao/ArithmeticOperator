ae9e0a1d93760028c89fe5ee9ceb3560e3b221bc
Eason Shao <ShaoY@StPaulsSchool.org.uk>
2023-09-12 10:15:52 +0100

    update Task 1.

--- ArithmeticOperator/Program.cs
+++ ArithmeticOperator/Program.cs
@@ -4,7 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Task 1 Circle Circumference
+
+        Console.WriteLine("Please input the radius of a circle: ");
+        double input = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"The circumference of the circle is {CircleCircumference(input).ToString("F2")}.");
+
+        Console.ReadLine();
+
+        
+    }
+
+    static double CircleCircumference(double radius)
+    {
+        return 2 * Math.PI * radius;
     }
 }
 
